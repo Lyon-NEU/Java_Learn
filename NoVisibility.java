911a2c40acebe53f529419b6297bdab66d82896e
Lyon-NEU <wanlplab@gmail.com>
2014-06-11 22:48:15 +0800

    memory share

---
+++ NoVisibility.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class NoVisibility{
+	private static boolean ready;
+	private static int number;
+	private class ReaderThread extends Thread{
+		public void run(){
+			while(!ready)
+				System.yield();
+			System.out.println(number);
+		}
+	}
+	public static void main(String []args){
+		new ReaderThread().start();
+		ready=true;
+		number=32;
+	}
+}
