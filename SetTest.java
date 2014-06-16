63ffae9f775b23742da1854fb671778c4f19ec27
Lyon-NEU <wanlplab@gmail.com>
2014-06-16 23:57:47 +0800

    set test

---
+++ SetTest.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * 
+ * @author Lyon
+ *
+ */
+public class SetTest {
+	public static void main(String[]args){
+		Set<String>words=new HashSet<>();
+		long totalTIme=0;
+		Scanner in =new Scanner(System.in);
+		while(in.hasNext()){
+			String word=in.next();
+			long callTime=System.currentTimeMillis();
+			
+		}
+	}
+}
