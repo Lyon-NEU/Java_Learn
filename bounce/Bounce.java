00f8e2a8f594fc79291e7d5e9f86551b4b1b133c
Lyon-NEU <wanlplab@gmail.com>
2014-06-20 23:54:25 +0800

    bounce thread

---
+++ bounce/Bounce.java
@@ -0,0 +1,80 @@
+package bounce;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * 
+ * @author Lyon
+ * @see Lyon-NEU
+ */
+public class Bounce {
+	public static void main(String[]args){
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run(){
+				JFrame frame=new BounceFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+	}
+}
+/**
+ * The frame with ball componety and buttons
+ */
+class BounceFrame extends JFrame
+{
+	private BallComponent comp;
+	public static final int STEPS=1000;
+	public static final int DELAY=3;
+	/**
+	 * Constructs the frame with the componet for showing the bouncing ball and Start and Close buttons
+	 */
+	public BounceFrame(){
+		setTitle("Bounce");
+		comp=new BallComponent();
+		add(comp,BorderLayout.CENTER);
+		JPanel buttonPanel=new JPanel();
+		addButton(buttonPanel,"Start",new ActionListener(){
+			public void actionPerformed(ActionEvent event){
+				addBall();
+			}
+		});
+		addButton(buttonPanel,"Close",new ActionListener(){
+			public void actionPerformed(ActionEvent event){
+				System.exit(0);
+			}
+		});
+		add(buttonPanel,BorderLayout.SOUTH);
+		pack();
+	}
+	
+	/**
+	 * Adds a button to a container
+	 * @param c the container
+	 * @param title the button title
+	 */
+	public void addButton(Container c,String title,ActionListener listener){
+		JButton button=new JButton();
+		c.add(button);
+		button.addActionListener(listener);
+	}
+	
+	/**
+	 * Adds a bouncing ball to the panel and makes it bounce 1,000 times
+	 */
+	public void addBall(){
+		try{
+			Ball ball=new Ball();
+			comp.add(ball);
+			for(int i=1;i<=STEPS;i++){
+				ball.move(comp.getBounds());
+				comp.paint(comp.getGraphics());
+				Thread.sleep(DELAY);
+			}
+		}catch(InterruptedException e){
+			
+		}
+	}
+}
