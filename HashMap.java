ba53575c21a65237ca2c46b312b633a5d1ff4870
Lyon-NEU <wanlplab@gmail.com>
2016-06-02 22:43:34 +0800

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -40,3 +40,36 @@ public HashMap(int initialCapacity, float loadFactor) {
         }
         .......
     }
+    public V put(K key, V value) {
+        //当key为null，调用putForNullKey方法，保存null与table第一个位置中，这是HashMap允许为null的原因
+        if (key == null)
+            return putForNullKey(value);
+        //计算key的hash值
+        int hash = hash(key.hashCode());                  ------(1)
+        //计算key hash 值在 table 数组中的位置
+        int i = indexFor(hash, table.length);             ------(2)
+        //从i出开始迭代 e,找到 key 保存的位置
+        for (Entry<K, V> e = table[i]; e != null; e = e.next) {
+            Object k;
+            //判断该条链上是否有hash值相同的(key相同)
+            //若存在相同，则直接覆盖value，返回旧value
+            if (e.hash == hash && ((k = e.key) == key || key.equals(k))) {
+                V oldValue = e.value;    //旧值 = 新值
+                e.value = value;
+                e.recordAccess(this);
+                return oldValue;     //返回旧值
+            }
+        }
+        //修改次数增加1
+        modCount++;
+        //将key、value添加至i位置处
+        addEntry(hash, key, value, i);
+        return null;
+    }
+    static int hash(int h) {
+        h ^= (h >>> 20) ^ (h >>> 12);
+        return h ^ (h >>> 7) ^ (h >>> 4);
+    }
+    static int indexFor(int h, int length) {
+        return h & (length-1);
+    }
